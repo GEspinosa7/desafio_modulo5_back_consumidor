e14d2be5668b3fb6d5b62dc980fbd208f7ea7628
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2021-08-22 17:39:23 -0300

    update endereco validations

--- src/validations/schemas/schemaAddEndereco.js
+++ src/validations/schemas/schemaAddEndereco.js
@@ -2,7 +2,7 @@ const yup = require('../configuracoes');
 
 const schemaAddEndereco = yup.object().shape({
   endereco: yup.string().required(),
-  cep: yup.string().required(),
+  cep: yup.required(),
   complemento: yup.string()
 });
 
